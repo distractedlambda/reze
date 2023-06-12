8a1719839bb21a04481cc6c04e9450869d92d311
Lucas Myers <lucascharlesmyers@gmail.com>
2023-06-11 20:22:19 -0400

    WIP

--- build.zig
+++ build.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 const Build = std.Build;
 const CrossTarget = std.zig.CrossTarget;
+const NativeTargetInfo = std.zig.system.NativeTargetInfo;
 const OptimizeMode = std.builtin.OptimizeMode;
 const Step = Build.Step;
 const Target = std.Target;
@@ -103,14 +104,19 @@ const ThirdPartyLibInfo = struct {
 const Configurator = struct {
     build: *Build,
     target: CrossTarget,
+    target_info: NativeTargetInfo,
     optimize_mode: OptimizeMode,
     glfw_lib: ?*Step.Compile = null,
     freetype_lib: ?*Step.Compile = null,
+    fontconfig_lib: ?*Step.Compile = null,
+    expat_lib: ?*Step.Compile = null,
 
     fn init(b: *Build) @This() {
+        const target = b.standardTargetOptions(.{});
         return .{
             .build = b,
-            .target = b.standardTargetOptions(.{}),
+            .target = target,
+            .target_info = NativeTargetInfo.detect(target) catch @panic("failed to detect target"),
             .optimize_mode = b.standardOptimizeOption(.{}),
         };
     }
@@ -252,6 +258,134 @@ const Configurator = struct {
         return lib;
     }
 
+    fn definedIf(condition: bool) ?u1 {
+        return if (condition) 1 else null;
+    }
+
+    fn addExpat(self: *Configurator) *Step.Compile {
+        // FIXME: do a better job of detecting available things here
+
+        if (self.expat_lib) |it| return it;
+        const lib = self.createCLib("expat");
+        self.expat_lib = lib;
+
+        const with_libbsd = self.build.option(bool, "expat_with_libbsd", "") orelse
+            false;
+
+        const context_bytes = self.build.option(i32, "expat_context_bytes", "") orelse
+            1024;
+
+        const dtd = self.build.option(bool, "expat_dtd", "") orelse
+            true;
+
+        const ns = self.build.option(bool, "expat_ns", "") orelse
+            true;
+
+        const dev_urandom = self.build.option(bool, "expat_dev_urandom", "") orelse
+            self.target.isLinux();
+
+        const with_getrandom = self.build.option(bool, "expat_with_getrandom", "") orelse
+            dev_urandom;
+
+        const with_sys_getrandom = self.build.option(bool, "expat_with_sys_getrandom", "") orelse
+            dev_urandom;
+
+        const attr_info = self.build.option(bool, "expat_attr_info", "") orelse
+            false;
+
+        const large_size = self.build.option(bool, "expat_large_size", "") orelse
+            false;
+
+        const min_size = self.build.option(bool, "expat_min_size", "") orelse
+            false;
+
+        if (with_libbsd) {
+            lib.linkSystemLibrary("bsd");
+        }
+
+        if (large_size) {
+            lib.defineCMacro("XML_LARGE_SIZE", null);
+        }
+
+        if (min_size) {
+            lib.defineCMacro("XML_MIN_SIZE", null);
+        }
+
+        const config_header = self.build.addConfigHeader(.{
+            .style = .{
+                .cmake = .{
+                    .path = "third_party/libexpat/expat/expat_config.h.cmake",
+                },
+            },
+
+            .include_path = "expat_config.h",
+        }, .{
+            .BYTEORDER = switch (self.target_info.target.cpu.arch.endian()) {
+                .Little => @as(u32, 1234),
+                .Big => @as(u32, 4321),
+            },
+
+            .HAVE_ARC4RANDOM = self.target.isDarwin(),
+            .HAVE_ARC4RANDOM_BUF = self.target.isDarwin(),
+            .HAVE_DLFCN_H = 1,
+            .HAVE_FCNTL_H = 1,
+            .HAVE_GETPAGESIZE = 1,
+            .HAVE_GETRANDOM = definedIf(with_getrandom),
+            .HAVE_INTTYPES_H = 1,
+            .HAVE_LIBBSD = definedIf(with_libbsd),
+            .HAVE_MEMORY_H = 1,
+            .HAVE_MMAP = 1,
+            .HAVE_STDINT_H = 1,
+            .HAVE_STDLIB_H = 1,
+            .HAVE_STRINGS_H = 1,
+            .HAVE_STRING_H = 1,
+            .HAVE_SYSCALL_GETRANDOM = definedIf(with_sys_getrandom),
+            .HAVE_SYS_STAT_H = 1,
+            .HAVE_SYS_TYPES_H = 1,
+            .HAVE_UNISTD_H = 1,
+            .PACKAGE_NAME = "expat",
+            .PACKAGE_BUGREPORT = "expat-bugs@libexpat.org",
+            .PACKAGE_STRING = "expat 2.5.0",
+            .PACKAGE_TARNAME = "expat",
+            .PACKAGE_VERSION = "2.5.0",
+            .STDC_HEADERS = 1,
+            .WORDS_BIGENDIAN = definedIf(self.target_info.target.cpu.arch.endian() == .Big),
+            .XML_ATTR_INFO = definedIf(attr_info),
+            .XML_CONTEXT_BYTES = context_bytes,
+            .XML_DEV_URANDOM = definedIf(dev_urandom),
+            .XML_DTD = definedIf(dtd),
+            .XML_NS = definedIf(ns),
+            .off_t = .off_t,
+            .size_t = .size_t,
+        });
+
+        lib.addConfigHeader(config_header);
+        lib.addIncludePath("third_party/libexpat/expat/lib");
+
+        lib.installConfigHeader(config_header, .{});
+        lib.installHeader("third_party/libexpat/expat/lib/expat.h", "expat.h");
+        lib.installHeader("third_party/libexpat/expat/lib/expat_external.h", "expat_external.h");
+
+        lib.addCSourceFiles(&.{
+            "third_party/libexpat/expat/lib/xmlparse.c",
+            "third_party/libexpat/expat/lib/xmlrole.c",
+            "third_party/libexpat/expat/lib/xmltok.c",
+        }, &.{
+            "-fno-strict-aliasing",
+            "-fvisibility=hidden",
+        });
+
+        return lib;
+    }
+
+    fn addFontconfig(self: *Configurator) void {
+        if (self.fontconfig_lib) |it| return it;
+        const lib = self.createCLib("fontconfig");
+        self.fontconfig_lib = lib;
+
+        return lib;
+    }
+
     fn configureBuild(self: *Configurator) void {
         const unit_tests = self.build.addTest(.{
             .root_source_file = .{ .path = "src/reze/reze.zig" },
@@ -261,6 +395,7 @@ const Configurator = struct {
 
         unit_tests.linkLibrary(self.addGlfw());
         unit_tests.linkLibrary(self.addFreetype());
+        unit_tests.linkLibrary(self.addExpat());
 
         const test_step = self.build.step("test", "Run unit tests");
         test_step.dependOn(&self.build.addRunArtifact(unit_tests).step);
